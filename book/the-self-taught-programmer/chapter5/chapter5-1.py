bb89182203b1bdf8f0d10f6e90bd7785e0cb49e1
river3015 <aaxy8402@yahoo.co.jp>
2025-05-25 23:11:41 +0900

    chapter5-1

---
+++ book/the-self-taught-programmer/chapter5/chapter5-1.py
@@ -0,0 +1,17 @@
+# colors = ["red","blue","green"]
+#
+# guess = input("何色でしょう？入力してください：")
+#
+# if guess in colors:
+#     print("正解")
+# else:
+#     print("失敗")
+
+test_tuple = ("test")
+print(type(test_tuple))
+
+test_tuple2 = ("test2",)
+print(type(test_tuple2))
+
+test_tuple3 = "test3",
+print(type(test_tuple3))
