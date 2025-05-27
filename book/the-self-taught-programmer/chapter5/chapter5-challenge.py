111ae76d3b5de767742de132e93d4d1100bf3c43
river3015 <aaxy8402@yahoo.co.jp>
2025-05-28 01:12:59 +0900

    chapter5-challenge

---
+++ book/the-self-taught-programmer/chapter5/chapter5-challenge.py
@@ -0,0 +1,21 @@
+# 1
+nijigasaki_list = ["kasumin","yu","ayumu","setsuna","ai","ema","rinari-","kanata","shizuku","karin","ranju","shioriko","mia"]
+
+# 2
+location = [(135,10),(000,000)]
+
+# 3
+strong_card_dictionary = { "cost": 7, "color": "green", "name": "ema"}
+print(strong_card_dictionary)
+
+# 4
+query_key = input("keyを入力して:")
+print(strong_card_dictionary[query_key])
+
+# 5
+oshi = {"ranju": ["Eutopia", "5201314", "夜明珠(イエミンジュ)"]}
+
+# 6
+# setは要素の重複がなく順番もないリスト。波括弧であらわす。
+set1 = {"ranju","ranju","ayumu","setsuna","setsuna"}
+print(set1)
